e062cd22aaba6cd351bdaa1461d152bfb410d3a2
KRaudla <kasparraudla@gmail.com>
2017-11-25 13:16:57 +0200

    Feed example, verctical vs horizontal linearlayout

---
+++ Android-apps/Facebook-view/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Facebook_view
+{
+    [Activity(Label = "Facebook_view", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+           // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            //var button = FindViewById<ImageButton>(Resource.Id.txtName);
+        }
+    }
+}
+
